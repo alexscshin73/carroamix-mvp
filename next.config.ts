d66a41199530e88913ceeb5ad37fcbcb0fb8c1d9
alexscshin73 <sc.shin73@gmail.com>
2026-03-05 16:37:05 -0600

    fix pwa config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-// web/next.config.ts
 import type { NextConfig } from "next";
 import withPWAInit from "next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV !== "production",
 });
 
 const nextConfig: NextConfig = {
